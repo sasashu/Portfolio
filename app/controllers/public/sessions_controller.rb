e50a23a97a4d59d13c9a6dfe4051320f47b25e43
sasashu <112250615+sasashu@users.noreply.github.com>
2022-11-10 07:39:17 +0000

    [Add]退会処理の記述

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+    before_action :reject_user, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -24,6 +25,18 @@ class Public::SessionsController < Devise::SessionsController
 
   def after_sign_out_path_for(resource)
     root_path
+  end
+  
+  def reject_user
+    @user = User.find_by(email: params[:user][:email])
+    if @user
+      if @user.valid_password?(params[:user][:password]) && (@user.is_active == false)
+        flash[:alert] = "このアカウントは退会されています。再度ご登録をしてください。"
+        redirect_to new_user_registration_path
+      else
+        flash[:notice] = "項目を入力してください"
+      end
+    end
   end
   # protected
 
