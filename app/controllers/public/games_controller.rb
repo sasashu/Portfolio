53a5a8972a9feb40f1154e19ded0f0701a0ff8b5
sasashu <112250615+sasashu@users.noreply.github.com>
2022-11-13 18:58:33 +0000

    [Add]絞り込み検索導入中

--- app/controllers/public/games_controller.rb
+++ app/controllers/public/games_controller.rb
@@ -1,7 +1,12 @@
 class Public::GamesController < ApplicationController
+  before_action :set_game, only: [:edit, :update, :destroy]
+
   def index
-    # .page(params[:page])の後に.per(10)を追加する事で1ページあたりの表示を制限
-    @games = Game.page(params[:page]).per(10)
+    # タグidがあった場合にはタグidから検索できるようにして、タグに紐付くリストを取得
+    # タグidが渡されなければ　Game.allで取得
+    @games = params[:tag_id].present? ? Tag.find(params[:tag_id]).games : Game.all
+    # page(params[:page])の後に.per(10)を追加する事で1ページあたりの表示を制限
+    @games = @games.page(params[:page]).per(10)
     # 全件表示の為にアクションを作成
     @total_games = Game.all
   end
