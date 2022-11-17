85b7f9b30572896347849362315faea6b34c0963
sasashu <112250615+sasashu@users.noreply.github.com>
2022-11-17 14:33:51 +0000

    [Update]絞り込み・順表示機能改良

--- app/controllers/public/games_controller.rb
+++ app/controllers/public/games_controller.rb
@@ -2,22 +2,32 @@ class Public::GamesController < ApplicationController
   before_action :set_game, only: [:edit, :update, :destroy]
 
   def index
-# case文
-    # page(params[:page])の後に.per(10)を追加する事で1ページあたりの表示を制限
-    # 新着順のソート
-    if params[:sort] == 'rate'
+    # 同条件なのでcase文の方が適切
+    # 1行目でレビューのrateのあるレビューの平均を表示させ、
+    # 点数が無いレビューは2行目で.all - 1行目 で点数ありを引いて表示、
+    # 3行目で1行目に2行目をpushで追加し、flattenで配列の[ ] を平滑化させている。
+    case params[:sort]
+    when 'rate'
       sorted_game_ids = Review.group(:game_id).order('avg(rate) DESC').pluck(:game_id)
       no_review_game_ids = Game.ids - sorted_game_ids
       @games = Game.find(sorted_game_ids.push(no_review_game_ids).flatten)
+    when 'date_DESC'
+      @games = Game.all.order('created_at DESC')
+    when 'date_ASC'
+      @games = Game.all.order('created_at ASC')
+    when 'release_DESC'
+      @games = Game.all.order('release DESC')
     else
       @games = Game.all.order('created_at DESC')
     end
-    # タグidがあった場合にはタグidから検索できるようにして、タグに紐付くリストを取得
-    # タグidが渡されなければ　Game.allで取得
+    # タグを指定するとそれに紐付けられたタグを絞り込みで表示
+    # 順表示の下にタグ絞り込みを記述することで順序を変えた状態でタグ絞り込みを行なっている。
     if params[:tag_id].present?
       tag_game_ids = GameTag.where(tag_id: params[:tag_id]).pluck(:game_id)
       @games = Game.find(@games.pluck(:id) & tag_game_ids)
     end
+    # page(params[:page])の後に.per(10)を追加する事で1ページあたりの表示を制限
+    # findだと問題は無いが、where等の配列を持って来たいのでKaminariにarrayを追加で記述する。
     @games = Kaminari.paginate_array(@games).page(params[:page]).per(10)
 
   end
