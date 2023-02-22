13310949320f9a9aa35f488d587160286118eee8
sasashu <112250615+sasashu@users.noreply.github.com>
2023-02-22 10:58:47 +0000

    [Update]インデント修正

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -9,41 +9,41 @@ let index;
 
 // reveiwの投稿ページに遷移するとstars.lengthの値（5）だけ繰り返し
 for(let i=0; i<stars.length; i++){
-	// 星にカーソルが乗ったときに実行する関数を定義
-	stars[i].addEventListener("mouseover",function(){
-		// stars.lengthの値（5）だけ繰り返し
-		// つまり、星カーソルが乗った時、5回繰り返される
-		for(let j=0; j<stars.length; j++){
-			// まず全て星をくり抜く
-			stars[j].classList.remove("fa-solid");
-			stars[j].classList.add("fa-regular");
-		}
-		// そのあと星の数だけ以下の関数が繰り返される。
-		for(let j=0; j<=i; j++){
-			stars[j].classList.remove("fa-regular");
-			// カーソルが乗った星まで星を塗りつぶす
-			stars[j].classList.add("fa-solid");
-		}
-	})
-		// クリックされた星の番号+1をratingValueに代入
-	stars[i].addEventListener("click",function(){
-		ratingValue.value = i+1;
-		ratingValueDisplay.textContent = ratingValue.value;
-		// indexにクリックされた星の番号を代入
-		index = i;
-	})
-	// 星からカーソルが離れたときに実行される関数
-	stars[i].addEventListener("mouseout",function(){
-		// まず5回繰り返す
-		for(let j=0; j<stars.length; j++){
-			// まず全ての星をくり抜く
-			stars[j].classList.remove("fa-solid");
-			stars[j].classList.add("fa-regular");
-		}
-		for(let j=0; j<=index; j++){
-			// クリックされている星まで塗りつぶす
-			stars[j].classList.remove("fa-regular");
-			stars[j].classList.add("fa-solid");
-		}
-	})
+  // 星にカーソルが乗ったときに実行する関数を定義
+  stars[i].addEventListener("mouseover",function(){
+    // stars.lengthの値（5）だけ繰り返し
+    // つまり、星カーソルが乗った時、5回繰り返される
+    for(let j=0; j<stars.length; j++){
+      // まず全て星をくり抜く
+      stars[j].classList.remove("fa-solid");
+      stars[j].classList.add("fa-regular");
+    }
+    // そのあと星の数だけ以下の関数が繰り返される。
+    for(let j=0; j<=i; j++){
+      stars[j].classList.remove("fa-regular");
+      // カーソルが乗った星まで星を塗りつぶす
+      stars[j].classList.add("fa-solid");
+    }
+  })
+    // クリックされた星の番号+1をratingValueに代入
+  stars[i].addEventListener("click",function(){
+    ratingValue.value = i+1;
+    ratingValueDisplay.textContent = ratingValue.value;
+    // indexにクリックされた星の番号を代入
+    index = i;
+  })
+  // 星からカーソルが離れたときに実行される関数
+  stars[i].addEventListener("mouseout",function(){
+    // まず5回繰り返す
+    for(let j=0; j<stars.length; j++){
+      // まず全ての星をくり抜く
+      stars[j].classList.remove("fa-solid");
+      stars[j].classList.add("fa-regular");
+    }
+    for(let j=0; j<=index; j++){
+      // クリックされている星まで塗りつぶす
+      stars[j].classList.remove("fa-regular");
+      stars[j].classList.add("fa-solid");
+    }
+  })
 }
