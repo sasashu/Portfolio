a59ebc30f4c96d7f7ed9cbaf5e7d5978a4b91166
sasashu <112250615+sasashu@users.noreply.github.com>
2023-04-08 09:53:19 +0000

    [Update]補足解説の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,10 @@ class User < ApplicationRecord
 
   has_many :reviews, dependent: :destroy
   has_many :comments, dependent: :destroy
-  
+
   validates :name, uniqueness: true
 
+  # ゲストログイン設定。名前とアドレスを自動入力、パスワードはランダム設定。
   def self.guest
     find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
